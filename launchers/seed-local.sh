a16a2f331317a500985778e4316be5c86f3043ed
Dani Marín <danimarin@blueroominnovation.com>
2025-10-24 18:30:59 +0200

    fix: update CREDENTIAL_SERVICE_BASE URL to match the correct port

--- launchers/seed-local.sh
+++ launchers/seed-local.sh
@@ -14,7 +14,7 @@ log() {
 }
 
 IDENTITY_API="http://localhost:9482/api/identity/v1alpha"
-CREDENTIAL_SERVICE_BASE="${CREDENTIAL_SERVICE_BASE:-http://host.docker.internal:9481}"
+CREDENTIAL_SERVICE_BASE="${CREDENTIAL_SERVICE_BASE:-http://host.docker.internal:9482}"
 CONTROLPLANE_BASE="${CONTROLPLANE_BASE:-http://host.docker.internal:9282}"
 PARTICIPANT_DID="${PARTICIPANT_DID:-did:web:host.docker.internal%3A9483}"
 SUPERUSER_API_KEY="${SUPERUSER_API_KEY:-c3VwZXItdXNlcg==.c3VwZXItc2VjcmV0LWtleQo=}"
